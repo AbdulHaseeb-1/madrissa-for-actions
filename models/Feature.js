daa9633c6ffabfd3094facea17d9cce822ae873e
Abdul Haseeb Windows <a.official.haseeb@gmail.com>
2025-07-28 22:33:12 +0500

    testing tags

--- models/Feature.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const featureSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // e.g., bayans, courses
-});
-
-module.exports = mongoose.model('Feature', featureSchema);
--- models/Role.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = new mongoose.Schema({
-  name: { type: String, required: true, enum: ["admin", "subadmin"] }, 
-  description: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Role', roleSchema);
